13deed778be3ace754bd0a8acf3551b8218f5efa
aizwellenstan <39293145+aizwellenstan@users.noreply.github.com>
2022-01-04 14:08:45 +0800

    Update restore-mysql.sh

--- restore-mysql.sh
+++ restore-mysql.sh
@@ -5,4 +5,7 @@ mv /var/lib/mysql /var/lib/mysql-old
 mkdir /var/lib/mysql
 mv ~/var/lib/mysql/* /var/lib/mysql
 chown -R mysql:mysql /var/lib/mysql
+
+cp /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld-$(date +%F).cnf
+cp mysql-disable-binlog.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 systemctl restart mysql
