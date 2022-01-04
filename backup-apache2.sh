a4ac27a34a5cbd51a69beb13d0241e1dc3a8756c
aizwellenstan <39293145+aizwellenstan@users.noreply.github.com>
2022-01-04 14:01:19 +0800

    Update backup-apache2.sh

--- backup-apache2.sh
+++ backup-apache2.sh
@@ -4,3 +4,4 @@ tar cfvz /opt/apache2-lib-backups/apache2-lib-$(date +%F).tar.gz /var/lib/apache
 
 mkdir /opt/apache2-conf-backups
 tar cfvz /opt/apache2-conf-backups/apache2-conf-$(date +%F).tar.gz /etc/apache2/*
+systemctl restart apache2
