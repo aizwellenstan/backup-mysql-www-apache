5d53f63789529dfa01f98a2a676bb2fa2385050f
aizwellenstan <39293145+aizwellenstan@users.noreply.github.com>
2022-01-04 13:56:47 +0800

    Update restore-www.sh

--- restore-www.sh
+++ restore-www.sh
@@ -1,7 +1,7 @@
 cd
 tar zxvf /opt/www-backups/www-archive.tar.gz -C .
-mv /var/www/html /var/www/html-old
-mkdir /var/www/html
-mv ~/var/www/html/* /var/www/html
-chown -R www-data:www-data /var/www/html
+mv /var/www /var/www-old
+mkdir /var/www
+mv ~/var/www/* /var/www
+chown -R www-data:www-data /var/www
 systemctl restart apache2
